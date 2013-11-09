a7b8b70769250f8e2f011e4b93c2571628fb1252
Dimitrios Archontis <d.archontis@gmail.com>
2013-11-09 14:40:48 +0200

    Update merosA_11.m

--- CE241/MEROS_A/merosA_11.m
+++ CE241/MEROS_A/merosA_11.m
@@ -1,42 +1,41 @@
-% MATlab Project
-% Part A // 1.1
+% Signals and Systems
+% Fall Semester 2011-12
+% Project Matlab
+% 1st Part // 1.1
 
 clc, close all, clear all
 
 help merosA_11
 
-l1=length('DIMITRIOS'); % Mikos Onomatos l1.
-l2=length('ARCHONTIS'); % Mikos Epithetou l2.
+l1=length('DIMITRIOS'); % Length Name l1.
+l2=length('ARCHONTIS'); % Length Surname l2.
  
-% Compute w1.
+% Calculation of w1
 ww1 = mod(10/11*(max(l1,l2))/(l1+l2),1);
 w1 = pi*ww1;
  
-% Compute w2. 
+% Calculation of w2
 ww2 = mod(ww1+1/4,1);
 w2 = pi*ww2;
 
-L=512;
+L=512;     % length signal is 512
 
-n=0:1:L-1; % Mikos tou n
+n=0:1:L-1; % length of n
  
-% Platoi Hmitonwn.
-A1=1; 
+A1=1;      % Platos tou 1ou cosine
+A2=0.75;   % Platos tou 2ou cosine
  
-A2=0.75;
+x1=A1*cos(w1*n);  % 1st cosine
+x2=A2*cos(w2*n);  % 2nd cosine
  
-x1=A1*cos(w1*n); % Sximatismos 1ou Hmitonou
+x0=x1+x2;  
  
-x2=A2*cos(w2*n); % Sximatismos 1ou Hmitonou
- 
-x0=x1+x2; %
- 
-w=(ones(1,L)); % 
+w=(ones(1,L)); 
 
-x=x1.*w+x2.*w; %
+x=x1.*w+x2.*w; % The input signal x[n]
 
-y=xcorr(x); % 
+y=xcorr(x); 
 
-stem(y); % 
-grid on %
-title('Autosysxetisi simatos x[n]');
+stem(y); % Plot the input signal x[n] in discrete time
+grid on
+title('Autocorrelation of input signal x[n]');
