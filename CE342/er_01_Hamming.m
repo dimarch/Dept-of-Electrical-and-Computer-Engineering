64ac17ab8d1542ade7f803935da094a4275da90a
Dimitrios Archontis <d.archontis@gmail.com>
2013-11-09 13:04:00 +0200

    Update and rename erwtima_01_1.m to er_01_Hamming.m

--- CE342/er_01_Hamming.m
+++ CE342/er_01_Hamming.m
@@ -7,29 +7,29 @@
 clc, close all, clear all
 
 % Comments in m-file
-help erwtima_01_1
+help er_01_Hamming
 
 % Design of lowpass FIR filter with Hamming window
 % Digital Filter Specifications:
-wp=0.2*pi;       % digital Passband freq in Hz
-ws=0.3*pi;       % digital Stopband freq in Hz 
+wp = 0.2*pi;     % digital Passband freq in Hz
+ws = 0.3*pi;     % digital Stopband freq in Hz 
 Rp = 0.25;       % Passband ripple in dB 
 As = 50;         % Stopband attenuation in dB
-tr_width=ws-wp;  % transition width
-M=ceil(6.6*pi/tr_width)+1;
+tr_width = ws-wp;% transition width
+M = ceil(6.6*pi/tr_width)+1;
 
-%disp(N)
-n=[0:1:M-1];
-wc=(ws+wp)/2;    % cutoff frequency of filter
-hd=ideal_lp(wc,M);
-w_ham=(hamming(M))';
-h=hd.*w_ham;
-[db,mag,phi,grd,w]=freqz_m(h,[1]);
-delta_w=2*pi/1000;
-Rp=-(min(db(1:1:wp/delta_w+1)));        % actual pass band ripple
-As=-round(max(db(ws/delta_w+1:1:501))); % Min stopband attenuation
+% disp(N)
+n = [0:1:M-1];
+wc = (ws+wp)/2;    % cutoff frequency of filter
+hd = ideal_lp(wc,M);
+w_ham = (hamming(M))';
+h = hd.*w_ham;
+[db,mag,phi,grd,w] = freqz_m(h,[1]);
+delta_w = 2*pi/1000;
+Rp = -(min(db(1:1:wp/delta_w+1)));        % actual pass band ripple
+As = -round(max(db(ws/delta_w+1:1:501))); % Min stopband attenuation
  
-%Plots 
+%Plot
 
 subplot(231)
 plot(w/pi,mag);
