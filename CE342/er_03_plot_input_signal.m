65587dd58473309f343e53ea684d499a4fe41190
Dimitrios Archontis <d.archontis@gmail.com>
2013-11-09 13:49:26 +0200

    Update er_03_plot_input_signal.m

--- CE342/er_03_plot_input_signal.m
+++ CE342/er_03_plot_input_signal.m
@@ -4,17 +4,17 @@
 
 clc, close all, clear all
 
-% Comments for m-file.
+% Comments for m-file
 help er_03_plot_input_signal
 
 l1 = length('DIMITRIOS'); % Length Name l1.
 l2 = length('ARCHONTIS'); % Length Surname l2.
  
-% Calculation of w1.
+% Calculation of w1
 ww1 = mod(10/11*(max(l1,l2))/(l1+l2),1);
 w1 = pi*ww1;
  
-% Calculation of w2. 
+% Calculation of w2
 ww2 = mod(ww1+1/4,1);
 w2 = pi*ww2;
 
@@ -28,7 +28,7 @@ A2 = 0.5;    % Platos tou 2ou cos
 x1 = A1*cos(w1*n); % 1st cosine
 x2 = A2*cos(w2*n); % 2nd cosine
  
-x = x1+x2;   % The signal x[n]
+x = x1+x2;   % The input signal x[n]
  
-stem(x);     % Plot in discrete time
+stem(x);     % Plot the input signal x[n] in discrete time
 grid on
