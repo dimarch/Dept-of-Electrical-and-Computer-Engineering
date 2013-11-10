7c59ffe35dad6603a64ba28a428a5a4f2aaa92fc
Dimitrios Archontis <d.archontis@gmail.com>
2013-11-10 17:35:14 +0200

    Update and rename merosA_er1_2.m to merosA_er1_2-DFT-256.m

--- CE241/MEROS_A/merosA_er1_2-DFT-256.m
+++ CE241/MEROS_A/merosA_er1_2-DFT-256.m
@@ -6,7 +6,7 @@
 clc, close all, clear all
 
 % Comments for m-file
-help merosA_er1_2
+help merosA_er1_2-DFT-256
 
 l1 = length('DIMITRIOS'); % Length Name l1.
 l2 = length('ARCHONTIS'); % Length Surname l2.
